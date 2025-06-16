e7bdd7b5a4426800b0cab4077ec31f723e1e81cc
AMIT <drmac@Macbook-Air-2.local>
2025-06-16 12:02:21 +0530

    changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,14 +1,30 @@
 import UIKit
 import Flutter
+import Firebase
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+  )  -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//      FirebaseApp.configure()
+      UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+              if granted {
+                print("Notification permission granted.")
+                DispatchQueue.main.async {
+                  application.registerForRemoteNotifications()
+                }
+              } else {
+                print("Notification permission denied.")
+              }
+            }
+            
+
+            // Set up Firebase Messaging delegate
+            UNUserNotificationCenter.current().delegate = self
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
 
